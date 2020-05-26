2f3295a56f017782a9298440d3c6904abfe91d85
Clarisse De Guzman <maryclarissedgzmn@gmail.com>
2020-05-25 22:58:24 -0400

    Initial Commit

---
+++ Lottery649/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lottery649
+//
+//  Created by Claire De Guzman on 2020-05-25.
+//  Copyright © 2020 Claire De Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
